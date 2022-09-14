8758d497af213ad67965d4a8f99a3e743ccd3f74
Pamela <abrepamm@gmail.com>
2022-09-14 11:20:44 -0300

    questão 6 pronta

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -3,7 +3,7 @@ const modelsSales = require('../models/salesModel');
 const newSale = async (sales) => {
   const id = await modelsSales.createSales(sales);
  
-  return { type: null, message: { id, itemSold: sales } };
+  return { type: null, message: { id, itemsSold: sales } };
 };
  
 module.exports = { newSale };
