d0139fe351a942b15f07e1c3d4d31b295bed526a
Pamela <abrepamm@gmail.com>
2022-09-14 13:22:02 -0300

    questão 8 mudando passando o  middlewares sales pela camada service

--- src/middlewares/sales/saleValid.js
+++ src/middlewares/sales/saleValid.js
@@ -1,11 +1,11 @@
 const mapError = require('../../utils/error');
-const { getByIdSales } = require('../../models/salesModel');
+const { getSales } = require('../../services/salesService');
 
 const validSaleById = async (req, res, next) => { 
   const { id } = req.params;
-  const validId = await getByIdSales(id);
+  const { message } = await getSales(id);
 
-  if (validId.length === 0) {
+  if (message.length === 0) {
     return res
       .status(mapError('NOT_FOUND'))
       .json({ message: 'Sale not found' });
